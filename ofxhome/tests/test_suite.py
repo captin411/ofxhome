d0831a8e44d3c5e18cd46ffb9009ec0195dfc762
David Bartle <davidb@mediatemple.net>
2016-02-10 12:03:33 -0800

    assertEquals is deprecated

--- ofxhome/tests/test_suite.py
+++ ofxhome/tests/test_suite.py
@@ -9,48 +9,48 @@ class InstitutionTestCase(unittest.TestCase):
         with testfile('scottrade.xml') as f:
             xml = f.read()
         i = Institution(xml)
-        self.assertEquals(i.id,'623')
-        self.assertEquals(i.name,'Scottrade, Inc.')
-        self.assertEquals(i.fid,'777')
-        self.assertEquals(i.org,'Scottrade')
-        self.assertEquals(i.brokerid,'www.scottrade.com')
-        self.assertEquals(i.url,'https://ofxstl.scottsave.com')
-        self.assertEquals(i.ofxfail,'0')
-        self.assertEquals(i.sslfail,'4')
-        self.assertEquals(i.lastofxvalidation,datetime.datetime(2012,8,13,22,28,10))
-        self.assertEquals(i.lastsslvalidation,datetime.datetime(2011,9,28,22,22,22))
-        self.assertEquals(i.xml, xml)
+        self.assertEqual(i.id,'623')
+        self.assertEqual(i.name,'Scottrade, Inc.')
+        self.assertEqual(i.fid,'777')
+        self.assertEqual(i.org,'Scottrade')
+        self.assertEqual(i.brokerid,'www.scottrade.com')
+        self.assertEqual(i.url,'https://ofxstl.scottsave.com')
+        self.assertEqual(i.ofxfail,'0')
+        self.assertEqual(i.sslfail,'4')
+        self.assertEqual(i.lastofxvalidation,datetime.datetime(2012,8,13,22,28,10))
+        self.assertEqual(i.lastsslvalidation,datetime.datetime(2011,9,28,22,22,22))
+        self.assertEqual(i.xml, xml)
 
     def testOptionalBroker(self):
         with testfile('jpmorgan.xml') as f:
             xml = f.read()
         i = Institution(xml)
-        self.assertEquals(i.id,'435')
-        self.assertEquals(i.name,'JPMorgan Chase Bank')
-        self.assertEquals(i.fid,'1601')
-        self.assertEquals(i.org,'Chase Bank')
-        self.assertEquals(i.brokerid,'')
-        self.assertEquals(i.url,'https://www.oasis.cfree.com/1601.ofxgp')
-        self.assertEquals(i.ofxfail,'0')
-        self.assertEquals(i.sslfail,'0')
-        self.assertEquals(i.lastofxvalidation,datetime.datetime(2014,8,17,22,23,35))
-        self.assertEquals(i.lastsslvalidation,datetime.datetime(2014,8,17,22,23,34))
-        self.assertEquals(i.xml, xml)
+        self.assertEqual(i.id,'435')
+        self.assertEqual(i.name,'JPMorgan Chase Bank')
+        self.assertEqual(i.fid,'1601')
+        self.assertEqual(i.org,'Chase Bank')
+        self.assertEqual(i.brokerid,'')
+        self.assertEqual(i.url,'https://www.oasis.cfree.com/1601.ofxgp')
+        self.assertEqual(i.ofxfail,'0')
+        self.assertEqual(i.sslfail,'0')
+        self.assertEqual(i.lastofxvalidation,datetime.datetime(2014,8,17,22,23,35))
+        self.assertEqual(i.lastsslvalidation,datetime.datetime(2014,8,17,22,23,34))
+        self.assertEqual(i.xml, xml)
 
     def testFromFile(self):
         i = Institution.from_file( testfile_name('scottrade.xml') )
-        self.assertEquals(i.id,'623')
-        self.assertEquals(i['id'],'623')
+        self.assertEqual(i.id,'623')
+        self.assertEqual(i['id'],'623')
 
     def testDictKeys(self):
         with testfile('scottrade.xml') as f:
             xml = f.read()
         i = Institution(xml)
-        self.assertEquals(i['id'],'623')
-        self.assertEquals(i['name'],'Scottrade, Inc.')
+        self.assertEqual(i['id'],'623')
+        self.assertEqual(i['name'],'Scottrade, Inc.')
 
         i['id'] = '123'
-        self.assertEquals(i['id'],'123')
+        self.assertEqual(i['id'],'123')
 
     def testBadParse(self):
         with testfile('badxml_bank.xml') as f:
@@ -65,23 +65,23 @@ class InstitutionListTestCase(unittest.TestCase):
 
     def testFromFile(self):
         l = InstitutionList.from_file( testfile_name('search_america.xml') )
-        self.assertEquals(len(l),15)
+        self.assertEqual(len(l),15)
 
     def testGoodResult(self):
         with testfile('search_america.xml') as f:
             xml = f.read()
         l = InstitutionList(xml)
-        self.assertEquals(len(l),15)
-        self.assertEquals(l.xml,xml)
-        self.assertEquals(l[0]['id'],'533')
-        self.assertEquals(l[0]['name'],'America First Credit Union')
+        self.assertEqual(len(l),15)
+        self.assertEqual(l.xml,xml)
+        self.assertEqual(l[0]['id'],'533')
+        self.assertEqual(l[0]['name'],'America First Credit Union')
 
     def testResultWithPHPError(self):
         with testfile('search_noexist.xml') as f:
             xml = f.read()
         l = InstitutionList(xml)
-        self.assertEquals(len(l),0)
-        self.assertEquals(l.xml,xml)
+        self.assertEqual(len(l),0)
+        self.assertEqual(l.xml,xml)
 
     def testIterator(self):
         count = 0
@@ -90,7 +90,7 @@ class InstitutionListTestCase(unittest.TestCase):
         l = InstitutionList(xml)
         for i in l:
             count += 1
-        self.assertEquals(count,15)
+        self.assertEqual(count,15)
 
     def testBadXML(self):
         with testfile('badxml_search.xml') as f:
